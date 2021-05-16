b5a3105806fe3471bf666181dd2de8fc23861139
AhmetcaN BOYRAZ <a.c.boyraz@hotmail.com.tr>
2021-05-16 15:00:19 +0300

    Bölüm 10 sonu

---
+++ Bölüm-10 -proje/bolum-10-urun-ekleme-template/urun-ekleme-template/src/components/Products.vue
@@ -0,0 +1,49 @@
+<template>
+    <div v-if="productList.length > 0">
+        <h3 class="text-center">Eklenen Ürünlerin Listesi</h3>
+        <hr>
+        <div class="row product-container">      
+          <app-product v-for="product in productList">
+            <img class="card-img-top" :src="product.selectedImage" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">{{product.title}}</h5>
+                <small>
+                    <strong>Adet : </strong> {{product.count}}
+                </small>
+                <br>
+                <small>
+                    <strong>Fiyat : </strong> {{product.price}}
+                </small>
+                <br>
+                <small>
+                    <strong>Tutar : </strong> {{product.total}}
+                </small>
+                </div>            
+          </app-product>
+        </div>
+    </div>
+</template>
+<script>
+import Product from './Product';
+import {eventBus} from '../main';
+
+export default {
+    data(){return {productList:[]}},
+   components:{
+       appProduct : Product
+   },
+   created(){
+       eventBus.$on("productAdded", (product)=>{
+           if(this.productList.length < 3){
+                this.productList.push(product);
+                eventBus.$emit("progressBarUpdated", this.productList.length);
+           }
+           else{
+               alert("Daha fazla ürün ekleyemezsiniz!!!");
+           }
+           
+           
+       });
+   }
+}
+</script>
---
+++ Bölüm-10 -proje/bolum-10-urun-ekleme-template/urun-ekleme-template/src/components/Progress.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="progress">
+      <div class="progress-bar bg-info" role="progressbar" :style="{width: progress +'%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+        {{currentItem}}/10
+      </div>
+    </div>
+</template>
+<script>
+import {eventBus} from '../main';
+
+export default{
+  data(){
+    return {currentItem : null, progress : 0}
+  },
+  created(){
+    eventBus.$on("progressBarUpdated", (productLength) => {
+      this.currentItem = productLength;
+      this.progress = productLength * 10; 
+    });
+  }
+}
+</script>
